e131cc5617f0931c26dc3135e9709808c79b65e6
Kalu Ogbonna <kaluogbonna48clear>
2022-09-08 16:01:13 -0700

    added shell script - C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function entry point
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	puts("\"Program5ming is like building a multilingual puzzle");
+	return (0);
+}
