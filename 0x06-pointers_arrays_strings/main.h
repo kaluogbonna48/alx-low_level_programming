1adf67355bff43074640a21898e8261a34598f07
Kalu Ogbonna <kaluogbonna48clear>
2022-09-22 21:30:25 -0700

    Added - task 12 file '104-print_buffer.c' and updated 'main.h'

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void rev_string(char *n);
+void print_buffer(char *b, int size);
+void print_line(char *c, int s, int l);
 
 #endif
